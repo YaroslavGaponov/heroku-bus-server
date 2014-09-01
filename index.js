c1b154d1bd52d5ba6ad0057b175da344a5be9db6
Yaroslav Gaponov <yaroslav.gaponov@gmail.com>
2014-09-01 11:54:03 +0300

    chnaged

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-var express = require('express');
-
-var PORT = process.env.PORT || 5000;
-
-var app = express();
-app.set('port', 80)
-
-app.get('/', function(request, response) {
-    response.send('Heroku Bus server is running at :' + PORT);    
-});
-
-app.listen(app.get('port'), function() {
-    console.log('Heroku Bus server is running at :' + PORT)
-});
+require('./web.js');
+require('./broker.js');
